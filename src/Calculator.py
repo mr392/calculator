44dab8447d2ca03f8de54398e5a336b779987cf5
Mike Rock <mr392@njit.edu>
2020-06-20 23:16:09 -0400

    Feature: Setup addition methods and test

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,15 +1,11 @@
-def subtraction (val1, val2):
-    int(val1)
-    int(val2)
-    return  val1 - val2
+
 
 class Calculator:
-    result = 1
+    result = 4
 
     def __init__(self):
         pass
 
-    def subtract(self, a, b):
-        a = 3
-        b = 2
-        self.result = subtraction(a, b)
+    def add(self, a, b):
+        c = a + b
+        return c
--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -3,15 +3,21 @@ from Calculator import Calculator
 
 
 
+
 class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
-    def test_subtract(self):
+    def test_results_property_calculator(self):
         calculator = Calculator()
+        self.assertEqual(calculator.result, 4)
 
-        self.assertEqual(calculator.result, 1)
+
+    def test_add_method_calculatoe(self):
+        calculator = Calculator()
+        self.assertEqual(calculator.add(2,2), 4)
+
 
 if __name__ == '__main__':
-    unittest.main()
+   unittest.main()
