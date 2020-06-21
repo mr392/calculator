25683c6de2a6b35ec29231fae409e5c4004d4c11
Mike Rock <mr392@njit.edu>
2020-06-20 23:24:30 -0400

    Fix: addition method

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,11 +1,13 @@
+def addition(a, b):
+    return a + b
 
 
 class Calculator:
-    result = 4
+    result = 0
 
     def __init__(self):
         pass
 
     def add(self, a, b):
-        c = a + b
-        return c
+        self.result = a + b
+        return addition(a, b)
