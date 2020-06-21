4fab1d4f0be9f3158b600dd10243735cb4cbe36f
Mike Rock <mr392@njit.edu>
2020-06-20 23:55:05 -0400

    Fix: addition corrected spelling

--- src/Calculator.py
+++ src/Calculator.py
@@ -1,6 +1,8 @@
 def addition(a, b):
     return a + b
 
+def subtraction(a, b):
+    return a - b
 
 class Calculator:
     result = 0
@@ -9,5 +11,9 @@ class Calculator:
         pass
 
     def add(self, a, b):
-        self.result = a + b
-        return addition(a, b)
+        self.result = addition(a, b)
+        return self.result
+
+    def subtract(self, a, b):
+        self.result = a - b
+        return subtraction(a, b)
