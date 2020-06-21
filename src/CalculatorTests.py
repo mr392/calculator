8dfafd775b8c2bebd9ac33a2002c47facdaad923
Mike Rock <mr392@njit.edu>
2020-06-20 21:47:49 -0400

    Feature: Setup first subtraction test

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -1,11 +1,17 @@
 import unittest
 from Calculator import Calculator
 
+
+
 class MyTestCase(unittest.TestCase):
     def test_instantiate_calculator(self):
         calculator = Calculator()
         self.assertIsInstance(calculator, Calculator)
 
+    def test_subtract(self):
+        calculator = Calculator()
+
+        self.assertEqual(calculator.result, 1)
 
 if __name__ == '__main__':
     unittest.main()
