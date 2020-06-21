0ff5648591b913a567932387fe7b920da0cdb6cb
Mike Rock <mr392@njit.edu>
2020-06-21 11:26:20 -0400

    Fix: changed csv function

--- src/CalculatorTests.py
+++ src/CalculatorTests.py
@@ -4,10 +4,10 @@ from Calculator import Calculator
 
 
 data = []
-def csv_values():
+def csv_values(filename):
 
 
-    with open("/src/Unit Test Addition.csv") as text_data:
+    with open(filename) as text_data:
 
         reader = csv.DictReader(text_data, delimiter=",")
 
@@ -35,12 +35,12 @@ class MyTestCase(unittest.TestCase):
 
 
     def test_add_method_calculator(self):
-           test_data = csv_values()
+           addition_test_data = csv_values("/src/Unit Test Addition.csv")
 
-           for number in test_data:
+           for number in addition_test_data:
               self.assertEqual(self.calculator.add(number['Value 1'], number['Value 2']), int(number['Result']))
-           #    #self.assertEqual(self.calculator.result, 4)
-              print (number)
+              self.assertEqual(self.calculator.result, int(number['Result']))
+
 
 
 
