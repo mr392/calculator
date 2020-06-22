8ce83ba02431ea9962497ebde4a88b758f20a737
Mike Rock <mr392@njit.edu>
2020-06-21 22:37:58 -0400

    Fix : rest of static methods

--- src/Calculator.py
+++ src/Calculator.py
@@ -3,20 +3,8 @@ from math import sqrt
 
 
 
-def subtraction(a, b):
-    return b - a
 
-def multiplication(a, b):
-    return a * b
 
-def division(a, b):
-    return b / a
-
-def squared(a):
-    return  a * a
-
-def square_root_of_number(a):
-    return sqrt(a)
 
 
 
@@ -31,25 +19,45 @@ class Calculator:
         return self.result
 
     def subtract(self, a, b):
-        self.result = subtraction(a, b)
+        self.result = Calculator.subtraction(a, b)
         return self.result
 
     def multiply(self, a,b):
-        self.result = multiplication(a, b)
+        self.result = Calculator.multiplication(a, b)
         return self.result
 
     def divide(self, a,b):
-        self.result = division(a, b)
+        self.result = Calculator.division(a, b)
         return self.result
 
     def square(self, a):
-        self.result = squared(a)
+        self.result = Calculator.squared(a)
         return self.result
 
     def square_root(self, a):
-        self.result = square_root_of_number(a)
+        self.result = Calculator.square_root_of_number(a)
         return self.result
 
     @staticmethod
     def addition(a, b):
         return a + b
+
+    @staticmethod
+    def subtraction(a, b):
+        return b - a
+
+    @staticmethod
+    def multiplication(a, b):
+        return a * b
+
+    @staticmethod
+    def division(a, b):
+        return b / a
+
+    @staticmethod
+    def squared(a):
+        return a * a
+
+    @staticmethod
+    def square_root_of_number(a):
+        return sqrt(a)
